af37a45db4e8f5c72797adb823a4e6eb69f2a77a
happyness980817 <happyness980817@gmail.com>
2023-11-11 19:43:48 +0900

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -3,36 +3,36 @@ import streamlit as st
 st.title('Welcome to CounsellingGPT!')
 st.subheader('This is merely a pretoype version.')
 
-# Initialize session state for storing chat
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
+# # Initialize session state for storing chat
+# if 'chat_history' not in st.session_state:
+#     st.session_state['chat_history'] = []
 
-# Function to handle message sending
-def send_message(username, key):
-    user_message = st.session_state[key]
-    if user_message:  # Only send non-empty messages
-        st.session_state['chat_history'].append((username, user_message))
-        st.session_state[key] = ""  # Clear the input box after sending
+# # Function to handle message sending
+# def send_message(username, key):
+#     user_message = st.session_state[key]
+#     if user_message:  # Only send non-empty messages
+#         st.session_state['chat_history'].append((username, user_message))
+#         st.session_state[key] = ""  # Clear the input box after sending
 
-# Display the chat history
-def display_chat_history():
-    st.write("Chat History:")
-    for name, message in st.session_state['chat_history']:
-        st.text(f"{name}: {message}")
+# # Display the chat history
+# def display_chat_history():
+#     st.write("Chat History:")
+#     for name, message in st.session_state['chat_history']:
+#         st.text(f"{name}: {message}")
 
-# User input for Counselor
-st.text_input("Counselor, type your message here:", 
-              key="counselor_message", 
-              on_change=send_message, 
-              args=("Counselor", "counselor_message"), 
-              placeholder="Type here and press Enter to send")
+# # User input for Counselor
+# st.text_input("Counselor, type your message here:", 
+#               key="counselor_message", 
+#               on_change=send_message, 
+#               args=("Counselor", "counselor_message"), 
+#               placeholder="Type here and press Enter to send")
 
-# User input for Client
-st.text_input("Client, type your message here:", 
-              key="client_message", 
-              on_change=send_message, 
-              args=("Client", "client_message"), 
-              placeholder="Type here and press Enter to send")
+# # User input for Client
+# st.text_input("Client, type your message here:", 
+#               key="client_message", 
+#               on_change=send_message, 
+#               args=("Client", "client_message"), 
+#               placeholder="Type here and press Enter to send")
 
-# Display the chat history
-display_chat_history()
+# # Display the chat history
+# display_chat_history()
