93ee1a791d956b38fa262114e067030eb6e26e45
happyness980817 <happyness980817@gmail.com>
2023-11-11 18:59:55 +0900

    press enter to send messages

--- chat_app.py
+++ chat_app.py
@@ -6,26 +6,32 @@ st.title('Welcome to my CounsellingGPT pretotype webpage!')
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
-# Function to display the chat history
+# Function to handle message sending
+def send_message(username, key):
+    user_message = st.session_state[key]
+    if user_message:  # Only send non-empty messages
+        st.session_state['chat_history'].append((username, user_message))
+        st.session_state[key] = ""  # Clear the input box after sending
+
+# Display the chat history
 def display_chat_history():
     st.write("Chat History:")
     for name, message in st.session_state['chat_history']:
         st.text(f"{name}: {message}")
 
-# Function to handle message sending
-def send_message(username, user_message):
-    if user_message:  # Only send non-empty messages
-        st.session_state['chat_history'].append((username, user_message))
-
 # User input for Counselor
-counselor_message = st.text_input("Counselor, type your message here:", key="counselor_message")
-if st.button('Send (Counselor)', key="send_counselor"):
-    send_message("Counselor", counselor_message)
+st.text_input("Counselor, type your message here:", 
+              key="counselor_message", 
+              on_change=send_message, 
+              args=("Counselor", "counselor_message"), 
+              placeholder="Type here and press Enter to send")
 
 # User input for Client
-client_message = st.text_input("Client, type your message here:", key="client_message")
-if st.button('Send (Client)', key="send_client"):
-    send_message("Client", client_message)
+st.text_input("Client, type your message here:", 
+              key="client_message", 
+              on_change=send_message, 
+              args=("Client", "client_message"), 
+              placeholder="Type here and press Enter to send")
 
 # Display the chat history
 display_chat_history()
