00898ad917c59d854def375030f348be3bcec4a4
happyness980817 <happyness980817@gmail.com>
2023-11-11 17:30:08 +0900

    Create chat_app.py

---
+++ chat_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# Initialize session state for storing chat
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+# User identification
+user_name = st.text_input("Enter your username:", key="username")
+
+# Chat input
+user_message = st.text_input("Type your message here:", key="message")
+
+# Button to send message
+if st.button('Send'):
+    # Append the username along with the message
+    st.session_state['chat_history'].append((user_name, user_message))
+
+# Display chat history
+st.write("Chat History:")
+for name, message in st.session_state['chat_history']:
+    st.text(f"{name}: {message}")
+
+
+
+    
