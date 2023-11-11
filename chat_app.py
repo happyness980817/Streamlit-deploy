739e6b1e54851fd0b8d964da2bfc6befd455f00e
happyness980817 <happyness980817@gmail.com>
2023-11-11 18:56:51 +0900

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -6,22 +6,26 @@ st.title('Welcome to my CounsellingGPT pretotype webpage!')
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
-# User identification
-user_name = st.text_input("Enter your username:", key="username")
+# Function to display the chat history
+def display_chat_history():
+    st.write("Chat History:")
+    for name, message in st.session_state['chat_history']:
+        st.text(f"{name}: {message}")
 
-# Chat input
-user_message = st.text_input("Type your message here:", key="message")
+# Function to handle message sending
+def send_message(username, user_message):
+    if user_message:  # Only send non-empty messages
+        st.session_state['chat_history'].append((username, user_message))
 
-# Button to send message
-if st.button('Send'):
-    # Append the username along with the message
-    st.session_state['chat_history'].append((user_name, user_message))
+# User input for Counselor
+counselor_message = st.text_input("Counselor, type your message here:", key="counselor_message")
+if st.button('Send (Counselor)', key="send_counselor"):
+    send_message("Counselor", counselor_message)
 
-# Display chat history
-st.write("Chat History:")
-for name, message in st.session_state['chat_history']:
-    st.text(f"{name}: {message}")
+# User input for Client
+client_message = st.text_input("Client, type your message here:", key="client_message")
+if st.button('Send (Client)', key="send_client"):
+    send_message("Client", client_message)
 
-
-
-    
+# Display the chat history
+display_chat_history()
