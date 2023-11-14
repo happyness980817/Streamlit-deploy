9d6615d0c425e4d1a0a0ee3d0005a753055994ab
happyness980817 <happyness980817@gmail.com>
2023-11-14 23:35:33 +0900

    chat history adjustment

--- chat_app.py
+++ chat_app.py
@@ -3,40 +3,6 @@ import streamlit as st
 st.title('Welcome to CounsellingGPT!')
 st.subheader('This is merely a pretoype version.')
 
-# # Initialize session state for storing chat
-# if 'chat_history' not in st.session_state:
-#     st.session_state['chat_history'] = []
-
-# # Function to handle message sending
-# def send_message(username, key):
-#     user_message = st.session_state[key]
-#     if user_message:  # Only send non-empty messages
-#         st.session_state['chat_history'].append((username, user_message))
-#         st.session_state[key] = ""  # Clear the input box after sending
-
-# # Display the chat history
-# def display_chat_history():
-#     st.write("Chat History:")
-#     for name, message in st.session_state['chat_history']:
-#         st.text(f"{name}: {message}")
-
-# # User input for Counselor
-# st.text_input("Counselor, type your message here:", 
-#               key="counselor_message", 
-#               on_change=send_message, 
-#               args=("Counselor", "counselor_message"), 
-#               placeholder="Type here and press Enter to send")
-
-# # User input for Client
-# st.text_input("Client, type your message here:", 
-#               key="client_message", 
-#               on_change=send_message, 
-#               args=("Client", "client_message"), 
-#               placeholder="Type here and press Enter to send")
-
-# # Display the chat history
-# display_chat_history()
-
 
 import streamlit as st
 import sqlite3
@@ -88,7 +54,7 @@ db = Database()
 
 # Initialize session state for storing chat
 if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = db.load_messages()
+    st.session_state['chat_history'] = []
 
 # User input for Counselor
 st.text_input("Counselor, type your message here:", 
